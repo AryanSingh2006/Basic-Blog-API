b38023b99155d70bd1336285472e501185aad3cb
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-05-03 16:09:00 +0530

    Connect the frontend

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import express from "express";
 import registerRoutes from './routes/register.routes.js';
 import loginRoutes from './routes/login.routes.js';
+import userRoutes from './routes/user.routes.js'
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views','./views')
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use('/user',registerRoutes);
-app.use('/user',loginRoutes);;
+app.use('/', userRoutes);
+app.use('/', registerRoutes)
+app.use('/',loginRoutes);;
 
 
 export default app;
