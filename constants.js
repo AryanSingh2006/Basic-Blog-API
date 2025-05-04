317b6a37e524ca4a0fa41d0d5ed8150be36f4fc8
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-05-04 12:43:54 +0530

    Made a seperate folder for all the constants used in the project(.env)

---
+++ constants.js
@@ -0,0 +1,4 @@
+export const MONGODB_URI = process.env.MONGODB_URI;
+export const PORT = process.env.PORT || 3000;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_EXPIRY = process.env.JWT_EXPIRY;
