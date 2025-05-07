5340ae76730bcc324e71e0a1d49928e6de6f406c
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-05-07 19:56:28 +0530

    Created auth.middleware.js

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,2 +1,27 @@
-import verifyToken from "../utils/jwt"
+import jwtUtil from "../utils/jwt.js";
 
+const authMiddleware = (req,res,next) => {
+  const token = req.cookies.token;
+
+  if (!token){
+    return res.status(401).json({
+      success:false,
+      message: "No token found"
+    });
+  }
+
+  try{
+    const decoded = jwtUtil.verifyToken(token);
+
+    req.user = decoded;
+  }
+  catch(err){
+    res.status(401).json({
+      success:false,
+      message:"Invalid token",
+      Error: err.message
+    })
+  }
+}
+
+export default authMiddleware;
