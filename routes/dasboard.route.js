2d69d0e8635a9c7aa3f697f3120e255df78fa473
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-05-07 19:57:10 +0530

    Added the authMiddleware

--- routes/dasboard.route.js
+++ routes/dasboard.route.js
@@ -1,8 +1,9 @@
 import express from "express";
 import renderdashboardPage from "../controllers/dashboard.controller.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
-router.get('/dashboard', renderdashboardPage);
+router.get('/dashboard', authMiddleware, renderdashboardPage);
 
 export default router;
