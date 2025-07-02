3f811000bc67a543afef95a4e5f5ab2f37d310d0
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-07-02 17:07:20 +0530

    Added User Model

--- models/user.model.js
+++ models/user.model.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema({
     type : String,
     required : [true,'Password is required'],
     trim : true
+  },
+  refreshToken: {
+    type: String,
+    default: ""
   }
 },
 {
