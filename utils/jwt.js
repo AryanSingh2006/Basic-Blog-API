7250be34298047dfccf0cb4e50d67836f139a3d7
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-07-03 16:58:42 +0530

    Working

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,15 +1,28 @@
 import jwt from "jsonwebtoken";
-import { JWT_EXPIRY, JWT_SECRET } from "../constants.js";
+import { 
+  JWT_EXPIRY,
+  JWT_SECRET,
+  JWT_REFRESH_SECRET,
+  JWT_REFRESH_EXPIRY
+} from "../constants.js";
 
-const generateToken = (payload) => {
+const generateAccessToken = (payload) => {
   return jwt.sign(payload, JWT_SECRET,{expiresIn: JWT_EXPIRY});
 }
+const generateRefreshToken = (payload) => {
+  return jwt.sign(payload, JWT_REFRESH_SECRET, {expiresIn: JWT_REFRESH_EXPIRY});
+}
 
-const verifyToken = (token) => {
+const verifyAccessToken = (token) => {
   return jwt.verify(token, JWT_SECRET);
+}
+const verifyRefreshToken = (token) => {
+  return jwt.verify(token, JWT_REFRESH_SECRET);
 }
 
 export default {
-  generateToken,
-  verifyToken
+  generateAccessToken,
+  verifyAccessToken,
+  generateRefreshToken,
+  verifyRefreshToken
 }
