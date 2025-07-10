d0541b414205a9f1206e369728771ac331d80542
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-07-10 16:17:32 +0530

    Added the Edit Update and Delete comtroller

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -13,10 +13,9 @@ const createBlog = async (req,res) => {
       message : "Content or Title is empty"
     })
   }
-  
   try{
     await blogModel.create({title, content, author : req.user.id});
-    res.redirect("/dashboard");
+    res.redirect("/homePage");
   }
   catch(err){
     return res.status(500).json({
@@ -25,7 +24,74 @@ const createBlog = async (req,res) => {
   }
 }
 
+const detailedBlog = async (req, res) => {
+  try {
+    const blog = await blogModel.findById(req.params.id).populate("author");
+    if (!blog) {
+      return res.status(404).send("Blog not found");
+    }
+    res.render("detailedBlog", { blog });
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+};
+
+const renderEditPage = async (req, res) => {
+  try {
+    const blog = await blogModel.findById(req.params.id);
+
+    if (!blog || blog.author.toString() !== req.user.id) {
+      return res.status(403).send("Unauthorized");
+    }
+
+    res.render("editBlog", { blog });
+  } catch (err) {
+    res.status(500).send("Error loading edit page");
+  }
+};
+
+const updateBlog = async (req, res) => {
+  const { title, content } = req.body;
+
+  try {
+    const blog = await blogModel.findById(req.params.id);
+
+    if (!blog || blog.author.toString() !== req.user.id) {
+      return res.status(403).send("Unauthorized");
+    }
+
+    blog.title = title;
+    blog.content = content;
+    await blog.save();
+
+    res.redirect("/profilePage");
+  } catch (err) {
+    res.status(500).send("Failed to update blog");
+  }
+};
+
+const deleteBlog = async (req, res) => {
+  try {
+    const blog = await blogModel.findById(req.params.id);
+
+    if (!blog || blog.author.toString() !== req.user.id) {
+      return res.status(403).send("Unauthorized");
+    }
+
+    await blog.deleteOne();
+    res.redirect("/profilePage");
+  } catch (err) {
+    res.status(500).send("Failed to delete blog");
+  }
+};
+
+
+
 export default {
   renderBlogpage,
-  createBlog
+  createBlog,
+  detailedBlog,
+  renderEditPage,
+  updateBlog,
+  deleteBlog
 }
