9615daf9ed860a30c2b7fe659eb323ee3dad1ae9
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-07-10 16:43:57 +0530

    Added the constants of refresh tokens annd access tokens

--- constants.js
+++ constants.js
@@ -2,3 +2,6 @@ export const MONGODB_URI = process.env.MONGODB_URI;
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const JWT_EXPIRY = process.env.JWT_EXPIRY;
+export const NODE_ENV = process.env.NODE_ENV || "development";
+export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+export const JWT_REFRESH_EXPIRY = process.env.JWT_REFRESH_EXPIRY;
