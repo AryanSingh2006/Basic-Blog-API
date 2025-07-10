308ba17520841dd836f3b1d317f0d91f24660b31
AryanSingh2006 <aryansingh18102006@gmail.com>
2025-07-10 16:42:56 +0530

    Update app.js with blog routes and middleware

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+
 import registerRoutes from './routes/register.routes.js';
 import loginRoutes from './routes/login.routes.js';
 import userRoutes from './routes/user.routes.js'
+import homepageRoutes from './routes/homePage.route.js'
+import logoutRoutes from "./routes/logout.routes.js"
+import refreshRoutes from "./routes/refreshToken.route.js"
+import blogRoutes from "./routes/blog.route.js"
+import profilePageRoutes from "./routes/profilePage.route.js"
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views','./views')
 
+app.use(express.static("public"));
+app.use(cookieParser());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 app.use('/', userRoutes);
-app.use('/', registerRoutes)
+app.use('/', registerRoutes);
 app.use('/',loginRoutes);;
+app.use('/', homepageRoutes);
+app.use('/', logoutRoutes);
+app.use('', refreshRoutes);
+app.use('', blogRoutes);
+app.use('', profilePageRoutes);
 
 
 export default app;
